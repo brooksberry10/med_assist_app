e12da21b1a18cb72916a64487d7a2cbc0bfe71ae
Nandu N <nn22e@fsu.edu>
2025-10-02 16:40:02 -0400

    chore(backend): enable Flask-Migrate and add initial migrations folder

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,14 +2,24 @@ from flask import Flask
 from .config import Config
 from .routes import bp as api_bp
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from .models import * 
 
 db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+
     app.register_blueprint(api_bp)
+ 
 
     @app.get("/")
     def root():
@@ -18,6 +28,7 @@ def create_app():
             200, 
             {"Content-Type": "text/plain; charset=utf-8"}
         )
+    
     
     return app
 
